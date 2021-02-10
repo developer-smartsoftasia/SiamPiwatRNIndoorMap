8d1044c47acaf4ac38d0d7b965aaeeb8263d8fe4
Toei <itthisakp@gmail.com>
2021-02-10 15:41:14 +0700

    0.1.9

---
+++ RNIndoorMap.xcframework/ios-arm64_armv7/RNIndoorMap.framework/Headers/RNIndoorMap.h
@@ -0,0 +1,19 @@
+//
+//  RNIndoorMap.h
+//  RNIndoorMap
+//
+//  Created by Itthisak Phueaksri on 13/1/2564 BE.
+//  Copyright © 2564 BE SmartSoftAsia. Co, Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "IndoorMapSDK-Bridging-Header.h"
+
+//! Project version number for RNIndoorMap.
+FOUNDATION_EXPORT double RNIndoorMapVersionNumber;
+
+//! Project version string for RNIndoorMap.
+FOUNDATION_EXPORT const unsigned char RNIndoorMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNIndoorMap/PublicHeader.h>
